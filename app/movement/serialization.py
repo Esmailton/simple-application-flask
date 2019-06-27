e707a1242877837e41b5ecef14edebada9ace405
Esmailton <buga_sg12@hotmail.com>
2019-06-26 21:52:53 -0300

    * correção em classes de serialização

--- app/movement/serialization.py
+++ app/movement/serialization.py
@@ -1,32 +1,41 @@
 from app import ma
-from .model import MovementModel, MovementEmployee, MovementDescription, MovementValue
-from marshmallow  import fields, schema
+from .model import MovementModel, MovementEmployeeModel, MovementDescriptionModel, MovementValueModel
+from marshmallow import fields
+from app.services.serialization import ServiceSchema
+from app.employee.serialization import EmployeeSchema
 
-class MovemenModeltSchema(ma.ModelSchema):
-    class Meta: 
-        model = MovementModel
+
+class MovementSchema(ma.ModelSchema):
+    class meta:
+        Model = MovementModel
 
     type = fields.Bool(required=True)
+    movement_employee = fields.Nested('MovementEmployeeSchema', many=True)
+    movement_value = fields.Nested('MovementValueSchema', many=True)
+    movement_description = fields.Nested(
+        'MovementDescriptionSchema', many=True)
+
 
 class MovementDescriptionSchema(ma.ModelSchema):
-    class Meta: 
-        model = MovementDescription
-    
-    movement_id = fields.Int(required=True)
+    class meta:
+        Model = MovementDescriptionModel
+    service = fields.Nested(ServiceSchema, only=['description'])
+    movement = fields.Nested(MovementSchema)
+
     expense_id = fields.Int(required=True)
     service_id = fields.Int(required=True)
 
-    
+
 class MovementEmployeeSchema(ma.ModelSchema):
-    class Meta: 
-        model = MovementEmployee
+
+    class meta:
+        model = MovementEmployeeModel
 
     employee_id = fields.Int(required=True)
-    movement_id = fields.Int(required=True)
+    employee = fields.Nested(EmployeeSchema, only=['name'])
+
 
 class MovementValueSchema(ma.ModelSchema):
-    class Meta: 
-        model = MovementValue
-
-    movement_id = fields.Int(required=True)
-    value = fields.Decimal(required=True)
+    class meta:
+        model = MovementValueModel
+    value = fields.Int(required=True)
