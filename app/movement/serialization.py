e3092f4dc90f911b545e88cdc1594f72a6a49c64
Esmailton <buga_sg12@hotmail.com>
2019-06-30 16:03:03 -0300

    Ajustando schema movimentações
      * Ajustando Schema das movimentações

--- app/movement/serialization.py
+++ app/movement/serialization.py
@@ -8,25 +8,30 @@ from ..Expense.serialization import ExpenseSchema
 
 class MovementSchema(ma.ModelSchema):
     class Meta:
-        Model = MovementModel
+        model = MovementModel
 
     type = fields.Bool(required=True)
 
-    movement_employee = fields.Nested('MovementEmployeeSchema', many=True)
-    movement_value = fields.Nested('MovementValueSchema', many=True)
+    movement_employee = fields.Nested(
+        'MovementEmployeeSchema', only=['employee'], many=True)
+    movement_value = fields.Nested(
+        'MovementValueSchema', only=['value'], many=True)
     movement_description = fields.Nested(
-        'MovementDescriptionSchema', many=True)
+        'MovementDescriptionSchema', only=['service', 'expense'], many=True)
 
 
 class MovementDescriptionSchema(ma.ModelSchema):
+
     class Meta:
-        Model = MovementDescriptionModel
+        model = MovementDescriptionModel
+
     service = fields.Nested(ServiceSchema, only=['description'])
     expense = fields.Nested(ExpenseSchema, only=['description'])
     movement = fields.Nested(MovementSchema)
 
     expense_id = fields.Int(required=True)
     service_id = fields.Int(required=True)
+    movement_id = fields.Int(required=True)
 
 
 class MovementEmployeeSchema(ma.ModelSchema):
@@ -36,9 +41,12 @@ class MovementEmployeeSchema(ma.ModelSchema):
 
     employee_id = fields.Int(required=True)
     employee = fields.Nested(EmployeeSchema, only=['name'])
+    movement_id = fields.Int(required=True)
 
 
 class MovementValueSchema(ma.ModelSchema):
     class Meta:
         model = MovementValueModel
+
     value = fields.Int(required=True)
+    movement_id = fields.Int(required=True)
